0f96679b7f0a38a6d1b68c318aa3297ff03b8667
hendisantika <hendisantika@yahoo.co.id>
2019-07-09 21:50:47 +0700

    #5 - Add VehicleService

---
+++ src/main/java/com/hendisantika/springbatchdemo/service/VehicleService.java
@@ -0,0 +1,14 @@
+package com.hendisantika.springbatchdemo.service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springbatch-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-07-09
+ * Time: 21:50
+ */
+public interface VehicleService {
+    boolean deleteAll();
+}
