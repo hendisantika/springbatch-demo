6383560a01b496b63fd3b606f48512b5458e9005
hendisantika <hendisantika@yahoo.co.id>
2019-07-10 05:41:18 +0700

    #15 - Add HomeController

---
+++ src/main/java/com/hendisantika/springbatchdemo/controller/HomeController.java
@@ -0,0 +1,39 @@
+package com.hendisantika.springbatchdemo.controller;
+
+import com.hendisantika.springbatchdemo.service.VehicleService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springbatch-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-07-10
+ * Time: 05:40
+ */
+@RestController
+public class HomeController {
+
+    private static final Logger log = LoggerFactory.getLogger(HomeController.class);
+
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @Qualifier("job1")
+    @Autowired
+    private Job job1;
+
+    @Qualifier("job2")
+    @Autowired
+    private Job job2;
+
+    @Autowired
+    private VehicleService vechileService;
+}
