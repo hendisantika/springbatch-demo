9553d5fb68de267a88566fd2b9f76fd74d4d4aa8
hendisantika <hendisantika@yahoo.co.id>
2019-07-10 05:41:41 +0700

    #16 - Add kickOffJob method in HomeController

--- src/main/java/com/hendisantika/springbatchdemo/controller/HomeController.java
+++ src/main/java/com/hendisantika/springbatchdemo/controller/HomeController.java
@@ -4,9 +4,12 @@ import com.hendisantika.springbatchdemo.service.VehicleService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -36,4 +39,21 @@ public class HomeController {
 
     @Autowired
     private VehicleService vechileService;
+
+    @GetMapping("/launchJob1")
+    public String kickOffJob() {
+
+        try {
+
+            JobParameters jobParameters = new JobParametersBuilder().addLong("time", System.currentTimeMillis()).toJobParameters();
+            jobLauncher.run(job1, jobParameters);
+
+        } catch (Exception e) {
+            log.info(e.getMessage());
+        }
+
+        return "Done";
+
+    }
+
 }
